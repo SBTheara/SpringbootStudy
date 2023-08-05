d15796ebcb074de93132bc9948444a2b29bdd8dd
SBTheara <thearasambath112@gmail.com>
2023-08-05 23:31:02 +0700

    lombok added

--- src/main/java/com/springboot/newSpring/ModelStudent/Student.java
+++ src/main/java/com/springboot/newSpring/ModelStudent/Student.java
@@ -1,57 +1,17 @@
 package com.springboot.newSpring.ModelStudent;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int StuId;
     private String StuName;
     private String StuSex;
     private double StuScore;
     private String StuAddress;
-    public Student(){}
-    public Student(int stuId, String stuName, String stuSex, double stuScore, String stuAddress) {
-        StuId = stuId;
-        StuName = stuName;
-        StuSex = stuSex;
-        StuScore = stuScore;
-        StuAddress = stuAddress;
-    }
-
-    public int getStuId() {
-        return StuId;
-    }
-
-    public void setStuId(int stuId) {
-        StuId = stuId;
-    }
-
-    public String getStuName() {
-        return StuName;
-    }
-
-    public void setStuName(String stuName) {
-        StuName = stuName;
-    }
-
-    public String getStuSex() {
-        return StuSex;
-    }
-
-    public void setStuSex(String stuSex) {
-        StuSex = stuSex;
-    }
-
-    public double getStuScore() {
-        return StuScore;
-    }
-
-    public void setStuScore(double stuScore) {
-        StuScore = stuScore;
-    }
-
-    public String getStuAddress() {
-        return StuAddress;
-    }
-
-    public void setStuAddress(String stuAddress) {
-        StuAddress = stuAddress;
-    }
 }
